8135e4a93f914cf2fcc38c98254eb583fb0e5369
Ho1lowHead <victor.arvidsson04@gmail.com>
2023-09-26 09:56:11 +0200

    boi

---
+++ js_grejor copy/JS/script.js
@@ -0,0 +1,8 @@
+let bruh = prompt("Vad heter du?")
+let lol = prompt("Hur många skor har du?")
+if (lol >= 10) {
+    alert("Niiiiiice " + lol + " skor är så coolt " + bruh + "!")
+}
+else {
+    alert("Du är en sån fucking noob " + bruh + "!")
+}
